fa87453f9132e3eed14009d2af034915bd6b4332
Sergey Kasitsin <kasitsinsergey@gmail.com>
2022-04-30 14:00:21 +0300

    Task_2 done

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 2. По заданному номеру дня недели вывести его название
+Console.WriteLine("Введите цифру от 1 до 7 ");
+int A = int.Parse(Console.ReadLine());
+if (A == 1) Console.WriteLine("Понедельник ");
+
+if (A == 2) Console.WriteLine("Вторник ");
+
+if (A == 3) Console.WriteLine("Среда ");
+
+if (A == 4) Console.WriteLine("Четверг ");
+
+if (A == 5) Console.WriteLine("Пятница ");
+
+if (A == 6) Console.WriteLine("Суббота ");
+
+if (A == 7) Console.WriteLine("Воскресенье ");
 
