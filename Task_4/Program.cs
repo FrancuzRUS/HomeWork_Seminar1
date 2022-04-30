0199e235e8ab22715bce1f554bf9b5698defc13b
Sergey Kasitsin <kasitsinsergey@gmail.com>
2022-04-30 15:17:31 +0300

    Task 4 done

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿// 4. Показать четные числа от 1 до N
+
+Console.WriteLine("Будем искать все четные числа от 1 до N. Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+
+for (int i=2; i <= N; i++)
+    if (i%2==0) Console.WriteLine(i);
